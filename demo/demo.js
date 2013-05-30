356b8cb10840cf46a6a0dae1a2c40f63b80f762e
Travis Wilson <travisw@exigo.com>
2013-05-30 13:29:49 -0700

    - Added the fluid grids

---
+++ demo/demo.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $('[class^="span"]').each(function (i, element) {
+        var $this = $(this);
+        var cssClass = $this.attr('class');
+        cssClass = cssClass.substring(4, cssClass.length);
+        $this.html(cssClass);
+    });
+});
